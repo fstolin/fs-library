1b3d7e7bd211dc60e7c71f5d5b177118e49ea233
gogomaester <stolin.f@gmail.com>
2023-01-23 21:23:00 +0100

    Docker not working

--- src/test/java/cz/uhk/kppro/fs/termwork/fslibrary/BookDAOImplTest.java
+++ src/test/java/cz/uhk/kppro/fs/termwork/fslibrary/BookDAOImplTest.java
@@ -3,16 +3,13 @@ package cz.uhk.kppro.fs.termwork.fslibrary;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.test.context.junit4.SpringRunner;
 
 import cz.uhk.kppro.fs.termwork.fslibrary.dao.BookDAOImpl;
 import cz.uhk.kppro.fs.termwork.fslibrary.entity.BookDetails;
 import jakarta.persistence.EntityManager;
 
-@RunWith(SpringRunner.class)
 @DataJpaTest
 public class BookDAOImplTest {
 	
--- src/test/java/cz/uhk/kppro/fs/termwork/fslibrary/CustomerDAOImplTest.java
+++ src/test/java/cz/uhk/kppro/fs/termwork/fslibrary/CustomerDAOImplTest.java
@@ -3,18 +3,15 @@ package cz.uhk.kppro.fs.termwork.fslibrary;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import cz.uhk.kppro.fs.termwork.fslibrary.dao.BookDAOImpl;
 import cz.uhk.kppro.fs.termwork.fslibrary.dao.CustomerDAOImpl;
-import cz.uhk.kppro.fs.termwork.fslibrary.entity.BookDetails;
 import cz.uhk.kppro.fs.termwork.fslibrary.entity.Customer;
 import jakarta.persistence.EntityManager;
 
-@RunWith(SpringRunner.class)
 @DataJpaTest
 class CustomerDAOImplTest {
 	
