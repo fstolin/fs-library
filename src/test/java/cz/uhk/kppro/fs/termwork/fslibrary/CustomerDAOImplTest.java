8b3b0ad45205dd5fedc15f4451aa41a9dbd9e4f2
gogomaester <stolin.f@gmail.com>
2023-01-23 18:47:13 +0100

    Docker, Tests

--- src/test/java/cz/uhk/kppro/fs/termwork/fslibrary/CustomerDAOImplTest.java
+++ src/test/java/cz/uhk/kppro/fs/termwork/fslibrary/CustomerDAOImplTest.java
@@ -2,51 +2,71 @@ package cz.uhk.kppro.fs.termwork.fslibrary;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.List;
-
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+import cz.uhk.kppro.fs.termwork.fslibrary.dao.BookDAOImpl;
 import cz.uhk.kppro.fs.termwork.fslibrary.dao.CustomerDAOImpl;
+import cz.uhk.kppro.fs.termwork.fslibrary.entity.BookDetails;
 import cz.uhk.kppro.fs.termwork.fslibrary.entity.Customer;
+import jakarta.persistence.EntityManager;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
 class CustomerDAOImplTest {
 	
 	// UNIT TEST EXECUTION
 	// Preparation - Execution - Assertion
 	
- 	@Autowired
-    private TestEntityManager entityManager;
-
     @Autowired
-    private CustomerDAOImpl customerDAO;
-
+    private EntityManager entityManager;
 	
     @Test
 	void getCustomersTest() {
 		// Preparation
 		//new DAO - autowired
 		//add customers
+    	BookDAOImpl bookDAO = new BookDAOImpl(entityManager);
     	
-		entityManager.persist(new Customer("Filip", "", ""));
-		entityManager.persist(new Customer("Petr", "", ""));
-		entityManager.persist(new Customer("Karel", "", ""));
+        entityManager.persist(new BookDetails("karel", "", 20, ""));
+        entityManager.persist(new BookDetails("karel2", "", 20, ""));
+
+
 		
 		// Execution
 		//DAO.getCustomers - vrati list
-		List<Customer> customers = customerDAO.getCustomers();
+        int size = bookDAO.getAllBookDetails().size();
+        System.out.println(size);
 		
 		// Assertion
 		//Assert
-		assertEquals(3, customers.size());
+		assertEquals(2, size);		
+	}
+    
+    @Test
+	void getCustomers2Test() {
+		// Preparation
+		//new DAO - autowired
+		//add customers
+    	CustomerDAOImpl custDAO = new CustomerDAOImpl(entityManager);
+    	
+        entityManager.persist(new Customer("Filip", "", ""));
+        entityManager.persist(new Customer("Karel", "", ""));
+        entityManager.persist(new Customer("Petr", "", ""));
+
+
 		
+		// Execution
+		//DAO.getCustomers - vrati list
+        int size = custDAO.getCustomers().size();
+		
+		// Assertion
+		//Assert
+		assertEquals(3, size);		
+		assertEquals("Karel", custDAO.getCustomer(2).getFirstName());
 	}
 	
 	
